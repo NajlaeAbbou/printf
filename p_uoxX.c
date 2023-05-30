d918d63e2c3c4a5c5839eb1a36d3ef6d9229c5d4
hadjabbou.najlae <hadjabbou.najlae@gmail.com>
2023-05-30 08:00:10 -0700

    corrections

--- p_uoxX.c
+++ p_uoxX.c
@@ -1,18 +1,60 @@
 #include "main.h"
-/* task2  b */
+
+/* task 3  u */
 
 /**
- * print_binary - prints number in binary  b ( base 2)
+ * print_unsignednumber - prints unsigned integer u
  * @v: va_list
- * @f: pointer to flag struct
+ * @f: pointer of flag
  * Return: number of chars
  */
-int print_binary(va_list v, char f[],
+
+int print_unsigned(va_list v, char f[],
 	int params, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
-	int count;
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(v, unsigned long int);
+	
+	UNUSED(f);
+	UNUSED(params);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	num = convertsizeunsgnd(num, size);
+
+	if (num == 0)
+		f[i--] = '0';
+
+	f[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		f[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (writeunsgnd(0, i, f, params, width, precision, size));
+}
+
+/*task 3 o */
+
+/**
+ * print_octal - prints a number in octal o (base 8)
+ * @v: va_list
+ * @f: pointer  flag
+ * Return: number of chars
+ */
+
+int print_octal(va_list v, char f[],
+	int params, int width, int precision, int size)
+{
+
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(v, unsigned long int);
+	unsigned long int initnum = num;
 
 	UNUSED(f);
 	UNUSED(params);
@@ -20,25 +62,97 @@ int print_binary(va_list v, char f[],
 	UNUSED(precision);
 	UNUSED(size);
 
-	n = va_arg(v, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
+	num = convertsizeunsgnd(num, size);
 
-			write(1, &z, 1);
-			count++;
-		}
+	if (num == 0)
+		f[i--] = '0';
+
+	f[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		f[i--] = (num % 8) + '0';
+		num /= 8;
 	}
-	return (count);
+
+	if (params & HASH && initnum != 0)
+		f[i--] = '0';
+
+	i++;
+
+	return (writeunsgnd(0, i, f, params, width, precision, size));
 }
+
+/**
+ * print_hexadecimal_lowercase- prints a number in hexadecimal x (base 16)
+ * @v: va_list arguments
+ * @f: pointer flag
+ * Return: number of chars
+ */
+
+int print_hexadecimal_lowercase(va_list v, char f[],
+	int params, int width, int precision, int size)
+{
+	return (printhexa(v, "0123456789abcdef", f,
+		params, 'x', width, precision, size));
+}
+
+/**
+ * print_hexadecimal_uppercase - prints a number in hexadecimal X (base 16).
+ * @v: va_list
+ * @f: pointer flag
+ * Return: number of chars
+ */
+int print_hexadecimal_uppercase(va_list v, char f[],
+	int params, int width, int precision, int size)
+{
+	return (printhexa(v, "0123456789ABCDEF", f,
+		params, 'X', width, precision, size));
+}
+
+/************** PRINT HEXX NUM IN LOWER OR UPPER **************/
+/**
+ * printhexa - Prints a hexadecimal number in lower or upper
+ * @v: Lista of arguments
+ * @map_to: Array of values to map the number to
+ * @f: f array to handle print
+ * Return: Number of chars printed
+ */
+int printhexa(va_list v, char map_to[], char f[],
+	int params, char flag, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(v, unsigned long int);
+	unsigned long int initnum = num;
+
+	UNUSED(f);
+	UNUSED(params);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	num = convertsizeunsgnd(num, size);
+
+	if (num == 0)
+		f[i--] = '0';
+
+	f[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		f[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (params & HASH && initnum != 0)
+	{
+		f[i--] = flag;
+		f[i--] = '0';
+	}
+
+	i++;
+
+	return (writeunsgnd(0, i, f, params, width, precision, size));
+}
+
 
